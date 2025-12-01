988f5809ce7e2cd42f0445c630a3cbd527142559
Pechersky Daniil <cdtt38.pecherskyD@yandex.ru>
2025-12-01 13:12:03 +0800

    Изменена основа

---
+++ src/core/reposity_keys.py
@@ -0,0 +1,96 @@
+
+class reposity_keys:
+    """
+    Class for storage reposity object keys 
+    """
+    
+    @staticmethod
+    def unit_key():
+        """
+        Function that returns data key for units
+        """
+        return "unit_model"
+    @staticmethod
+    def range_group_key():
+        """
+        Function that returns data key for range_groups
+        """
+        return "range_group_model"
+    @staticmethod
+    def range_key():
+        """
+        Function that returns data key for ranges
+        """
+        return "range_model"
+    @staticmethod
+    def receipt_key():
+        """
+        Function that returns data key for receipts
+        """
+        return "receipt_model"
+    
+    @staticmethod
+    def storage_key():
+        """
+        Function that returns data key for storages
+        """
+        return "storage_model"
+    @staticmethod
+    def transaction_key():
+        """
+        Function that returns data key for transactions
+        """
+        return "transaction_model"
+
+    @staticmethod
+    def remnant_key():
+        """
+        Function that returns data key for transactions
+        """
+        return "remnant_model"
+
+    @staticmethod
+    def unit_json_key():
+        """
+        Function that returns json key for units
+        """
+        return "units"
+    
+    @staticmethod
+    def range_group_json_key():
+        """
+        Function that returns json key for range_groups
+        """
+        return "range_groups"
+    @staticmethod
+    def range_json_key():
+        """
+        Function that returns json key for ranges
+        """
+        return "ranges"
+    @staticmethod
+    def receipt_json_key():
+        """
+        Function that returns json key for receipts
+        """
+        return "receipts"
+
+    @staticmethod
+    def storage_json_key():
+        """
+        Function that returns data key for storages
+        """
+        return "storages"
+    @staticmethod
+    def transcation_json_key():
+        """
+        Function that returns data key for transactions
+        """
+        return "transactions"
+
+    @staticmethod
+    def remnant_json_key():
+        """
+        Function that returns data key for transactions
+        """
+        return "remnants"
